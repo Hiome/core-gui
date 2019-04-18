9d544bbb68712e1719622a1e0f6ea8991113db69
Neil Gupta <neil@hiome.com>
2019-04-18 14:30:44 -0500

    drop machine id from mqtt topic

--- publishEvent.js
+++ publishEvent.js
@@ -1,10 +1,9 @@
-const MACHINEID = process.env.NODE_ENV === 'production' ? require('fs').readFileSync('/sys/class/net/eth0/address', 'utf8').trim() : 'local'
 const mqtt = require('mqtt')
 
 function publishEvent(msg) {
   if (process.env.NODE_ENV === 'production') {
-    const client = mqtt.connect('mqtt://localhost:1883', {clientId: `gui:${MACHINEID}`})
-    client.on('connect', () => client.publish(`hiome/1/gui:${MACHINEID}/event`, msg, () => client.end()))
+    const client = mqtt.connect('mqtt://localhost:1883')
+    client.on('connect', () => client.publish('hiome/1/api/event', msg, () => client.end()))
   }
 }
 
