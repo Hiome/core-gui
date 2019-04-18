031d0cb465e32b9ccb826b7e57a861af01cc76d3
Neil Gupta <neil@hiome.com>
2019-04-18 18:28:48 -0500

    simplify mqtt topic

--- publishEvent.js
+++ publishEvent.js
@@ -3,7 +3,7 @@ const mqtt = require('mqtt')
 function publishEvent(msg) {
   if (process.env.NODE_ENV === 'production') {
     const client = mqtt.connect('mqtt://localhost:1883')
-    client.on('connect', () => client.publish('hiome/1/api/event', msg, () => client.end()))
+    client.on('connect', () => client.publish('hiome/1/api', msg, () => client.end()))
   }
 }
 
