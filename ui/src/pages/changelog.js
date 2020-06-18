1360d90e88f0e9a6b4b328913e3ecca55467c83b
Neil Gupta <neil@hiome.com>
2020-06-18 12:34:22 -0500

    build public dir, add breaking changes to changelog

--- ui/src/pages/changelog.js
+++ ui/src/pages/changelog.js
@@ -11,16 +11,21 @@ const ChangelogPage = () => (
       <li><span role="img" aria-label="design">🎨</span> Redesigned dashboard</li>
       <li><span role="img" aria-label="book">📖</span> More detailed logs of what's happening in your home</li>
       <li><span role="img" aria-label="fire">🔥</span> Even faster responsiveness</li>
-      <li><span role="img" aria-label="coder">👩‍💻</span> Improved <Link to="/docs">API docs</Link></li>
-      <li><span role="img" aria-label="cancel">❌</span> Undo entries to revert them</li>
-      <li><span role="img" aria-label="brain">🧠</span> Improved error correction with AI that learns what's valid and what's noise in <em>your</em> home</li>
+      <li><span role="img" aria-label="cancel">❌</span> Undo false entries to revert them</li>
+      <li><span role="img" aria-label="brain">🧠</span> New AI for error correction</li>
     </ul>
 
     <p>
-      That last point is actually the most exciting. You can now <Link to="/door">train Hiome</Link> on which events to consider valid in order to improve accuracy.
-      Previously, Hiome relied on predefined thresholds to decide which events were valid or not, and the sensitivity slider could just adjust that slightly.
+      You can now <Link to="/door">train Hiome</Link> on which entries to count.
+      Previously, Hiome relied on predefined thresholds to decide whether an event was valid or not, and the sensitivity slider could just adjust that slightly.
       We have migrated to letting each sensor learn what a valid event looks like, specific to that door. This should provide a nice boost to accuracy!
     </p>
+
+    <p><strong>Breaking Changes</strong></p>
+    <ul>
+      <li>MQTT topics have changed. Please consult the new <Link to="/docs">API docs</Link> to update your custom scripts</li>
+      <li>IFTTT event names have slightly changed. Please see <Link to="/settings/integrations/ifttt">IFTTT integration</Link></li>
+    </ul>
   </Layout>
 )
 
