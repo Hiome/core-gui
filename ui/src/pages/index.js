e8bc62789e3adf8eee156dc235551eb737420b8a
Neil Gupta <neil@hiome.com>
2020-06-19 08:03:17 -0500

    drop surround div of spinner on index page

--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -50,10 +50,8 @@ class IndexPage extends Component {
   }
 
   renderRooms() {
-    if (this.state.loading) {
-      return <><div style={{textAlign: `center`, marginTop: `10em`}}>
-        <Spin size="large" indicator={<Icon type="loading" />} />
-      </div></>
+    if (this.state.loading || true) {
+      return <Spin size="large" indicator={<Icon type="loading" />} style={{textAlign: `center`, margin: `10em 0`, display: `block`}} />
     } else if (this.state.rooms.length > 0) {
       const arr = this.state.rooms.sort((a,b) => {
         if (a.occupancy_count === b.occupancy_count) {
