b6c3259d544f9916cd36a5f49312b1cc49125105
Neil Gupta <neil@hiome.com>
2020-06-21 10:12:41 -0500

    TimeAgo: don't trigger a setState right on mount

--- ui/src/components/TimeAgo.js
+++ ui/src/components/TimeAgo.js
@@ -5,14 +5,6 @@ import { Tooltip } from 'antd'
 import strftime from "./strftime"
 
 class TimeAgo extends PureComponent {
-  state = {
-    formattedTime: ''
-  }
-
-  componentDidMount() {
-    this.setState({formattedTime: this.format(this.props.time, this.props.debug)})
-  }
-
   componentWillUnmount() {
     if (this.timerHandler) {
       clearTimeout(this.timerHandler)
@@ -20,17 +12,12 @@ class TimeAgo extends PureComponent {
     }
   }
 
-  format(time, debug) {
-    const d = new Date(time)
-    if (debug) {
-      return strftime('%l:%M:%S.%f %p', d)
-    }
-
+  format(d) {
     const totalSec = Math.floor((new Date() - d)/1000)
     // set a timer to format this time again every 5 seconds
-    if (totalSec < 60) this.timerHandler = setTimeout(this.componentDidMount.bind(this), (5 - (totalSec % 5))*1000)
+    if (totalSec < 60) this.timerHandler = setTimeout(this.forceUpdate.bind(this), (5 - (totalSec % 5))*1000)
     // or every minute after the first 60 seconds
-    else if (totalSec < 300) this.timerHandler = setTimeout(this.componentDidMount.bind(this), 61000)
+    else if (totalSec < 300) this.timerHandler = setTimeout(this.forceUpdate.bind(this), 61000)
 
     if (totalSec < 5) return 'just now'
     if (totalSec < 60) return `${totalSec} sec ago`
@@ -42,19 +29,14 @@ class TimeAgo extends PureComponent {
     const d = new Date(this.props.time)
     return (
       <Tooltip title={strftime('%l:%M:%S %p', d)}>
-        <time dateTime={d}>{this.state.formattedTime}</time>
+        <time>{this.format(d)}</time>
       </Tooltip>
     )
   }
 }
 
 TimeAgo.propTypes = {
-  time: PropTypes.number.isRequired,
-  debug: PropTypes.bool
-}
-
-TimeAgo.defaultProps = {
-  debug: false
+  time: PropTypes.number.isRequired
 }
 
 export default TimeAgo
